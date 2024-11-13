851b7415197502a338c123632b8dede1eb17665d
Thushar <84955313+Thushar345@users.noreply.github.com>
2024-11-13 12:27:46 +0530

    resourceallocatedAPI created

---
+++ Rescueoperation/AllocateResourceAPI/Models/AllocateResource.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AllocateResourceAPI.Models
+{
+    public class AllocateResource
+    {
+        [Key]
+        public int AllocationId { get; set; }
+        public int IncidentId { get; set; }
+        public string IncidentName { get; set; }
+        public string IncidentType { get; set; }
+        public string Location { get; set; }
+        public string Severity { get; set; }
+        public List<AllocatedR> allocatedR { get; set; } = new List<AllocatedR>();
+    }
+
+ 
+}
---
+++ Rescueoperation/AllocateResourceAPI/Repository/IAllocateResourceRepository.cs
@@ -0,0 +1,16 @@
+﻿using AllocateResourceAPI.Models;
+
+namespace AllocateResourceAPI.Repository
+{
+    public interface IAllocateResourceRepository
+    {
+        // Get all resource allocations (with allocated resources)
+        Task<IEnumerable<AllocateResource>> GetResourceAllocateAsync();
+
+        // Get a specific resource allocation by allocationId (with allocated resources)
+        Task<AllocateResource> GetResourceAllocateByIdAsync(int allocationId);
+
+        // Add a new resource allocation (and its associated allocated resources)
+        Task AddResourceAllocateAsync(AllocateResource allocateResource);
+    }
+}
